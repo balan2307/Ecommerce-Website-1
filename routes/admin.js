5a3f00c3edd1e3eee37498606c57f15835e28d82
Varun Shinde <varunshinde.10601@gmail.com>
2021-08-16 18:13:19 +0530

    loggedIn middleware added

--- routes/admin.js
+++ routes/admin.js
@@ -1,32 +1,42 @@
 const express = require("express");
 const router = express.Router();
-const adminController = require("../controllers/adminController");
+const adminController = require("../controllers/productsController");
+const isLoggedIn = require("../middlewares/user_auth");
 
 /* GET users listing. */
-router.get("/products", adminController.ViewProducts);
-router.get("/product/:productId",adminController.SingleProduct)
+router.get("/products",isLoggedIn, adminController.ViewProducts);
+router.get("/product/:productId", isLoggedIn, adminController.SingleProduct);
 router.post(
   "/addProduct",
+  isLoggedIn,
   adminController.AddToMulter,
   adminController.AddProducts
 );
 
-router.post("/editProducts/:productId", adminController.AddToMulter, adminController.EditProducts);
+router.post(
+  "/editProducts/:productId",
+  isLoggedIn,
+  adminController.AddToMulter,
+  adminController.EditProducts
+);
 
-router.post("/deleteProducts/:productId", adminController.DeleteProduct);
+router.post(
+  "/deleteProducts/:productId",
+  isLoggedIn,
+  adminController.DeleteProduct
+);
 
-router.route('/register')
-.get((req,res)=>res.render('admin/register'))
-.post(adminController.register)
+router
+  .route("/register")
+  .get((req, res) => res.render("admin/register"))
+  .post(adminController.register);
 
-router.route('/login')
-.get((req,res)=>res.render('admin/login'))
-.post(adminController.login)
+router
+  .route("/login")
+  .get((req, res) => res.render("admin/login"))
+  .post(adminController.login);
 
-router.get('/logout',adminController.logout)
-router.get('/test',(req,res)=>
-{
-    res.send(req.session);
-})
+router.get("/logout", isLoggedIn, adminController.logout);
+
 
 module.exports = router;
