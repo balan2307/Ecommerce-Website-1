27be713f5083dd71f37371c479545e19b90b214f
ShyamSunderS <sshyamsub01871@gmail.com>
2021-08-20 20:05:35 +0530

    Added Landing Page

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const usersRouter = require("./users");
 const adminRouter = require('./admin');
 const storeRouter = require('./store');
+const landingRouter = require("./landingPage");
 
 // const firebase = require('../config/firebaseInit');
 // const db = firebase.firestore();/
@@ -12,13 +13,20 @@ const InitRoutes = (app) => {
   app.get("/", function (req, res, next) {
     res.render("index", { title: "Express" });
   });
+  app.get("/home",(req, res) =>
+  {
+    res.render("landingPage.ejs");
+  });
   app.get("/test", function (req, res, next) {
     res.render("test");
   });
 
+
+
   app.use("/users", usersRouter);
   app.use("/admin", adminRouter);
   app.use("/store",storeRouter);
+  app.use("/",landingRouter);
 
   console.log("Routes Initialized")
 
